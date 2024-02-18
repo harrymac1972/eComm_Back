be0204eaf3f8fcdd5c099b4101c40df58a4499b7
harry <harrymac1972@gmail.com>
2024-02-18 08:22:16 -0700

    '/api/tags' has json response

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
+
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// get all tags
+router.get('/', async (req, res) => {
+  try {
+    const tags = await Tag.findAll();
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
