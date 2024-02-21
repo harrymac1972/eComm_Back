c125b327e5921c6bc982c972a5bfc358b2b09dca
harry <harrymac1972@gmail.com>
2024-02-20 18:49:25 -0700

    explicit comments made to show lesson learned in models.index.js

--- models/index.js
+++ models/index.js
@@ -13,10 +13,12 @@ Product.belongsTo(Category, {foreignKey: 'category_id'});
 Category.hasMany(Product, {foreignKey: 'category_id'});
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag,{through: ProductTag,foreignKey: 'product_id'});
+Product.belongsToMany(Tag,{through: ProductTag, foreignKey: 'product_id'});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product,{through: ProductTag,foreignKey: 'tag_id'});
+Tag.belongsToMany(Product,{through: ProductTag, foreignKey: 'tag_id'});
 
+// NOTE:  incorrect error was:    {through: "ProductTag", ...
+//         proper is WITHOUT QUOTATION MARK so it pulls variable imported
 
 module.exports = {
   Product,
