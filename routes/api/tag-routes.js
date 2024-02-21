8813fae952d6c5100ae50508a740be042f48d72c
harry <harrymac1972@gmail.com>
2024-02-20 22:10:54 -0700

    tag update started

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,8 +41,23 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req,res) => {
+  try {
+    const updatedTag = await Tag.update(req.body,{
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!updatedTag) {
+      res.status(404).json({ message: 'Tag not found' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Tag updated' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
